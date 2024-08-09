91882011e4bb1bf4ba655c75b17463fe30b9654b
frpitu <94804469+frpitu@users.noreply.github.com>
2024-08-08 21:09:12 -0300

    Add files via upload

---
+++ Beautiful.py
@@ -0,0 +1,34 @@
+__description__ = 'Make your code beautiful'
+__author__ = 'frpitu'
+__version__ = '1.0'
+__date__ = '09/08/2024'
+
+"""
+
+      __            _ 
+    / _|_ __ _ __ (_) |_ _   
+   | |_| '__| '_ \| | __| | | |
+   |  _| |  | |_) | | |_| |_| |
+   |_| |_|  | .__/|_|\__|\__,_|
+            |_|
+ * Copyrigth frpitu 2024 todos os direitos reservados
+ * O projeto tem a lincensa GPL 2.0, caso você queira usar qualquer código está disponivel
+
+"""
+
+class Beautiful:
+  def act.in(text):
+    print("> Formatting code.. ", end="")
+    data = {
+      'data': self.source, 'indentation_style': 'k&r',
+      'indent_with': 'spaces', 'indentation_size': '4',
+      'ArrayNested': 'ArrayNested', 'EqualsAlign': '',
+      'Fluent': 'Fluent', 'KeepEmptyLines': 'KeepEmptyLines',
+      'ListClassFunction': '', 'Lowercase': 'Lowercase',
+    }
+      
+    try: 
+      self.source = requests.post('https://beautifytools.com/pb.php', data=data).text
+      print("done")
+    except:
+      print("\n[+] there was an error formatting... using original")
